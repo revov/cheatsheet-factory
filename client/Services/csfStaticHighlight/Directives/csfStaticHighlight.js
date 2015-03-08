d566c93a0b0103bd809c4f97a36a61859055c55f
Stoyan Revov <st.revov@gmail.com>
2015-03-08 02:04:58 +0200

    Improved Static Highlight performance

--- client/Services/csfStaticHighlight/Directives/csfStaticHighlight.js
+++ client/Services/csfStaticHighlight/Directives/csfStaticHighlight.js
@@ -4,28 +4,29 @@ angular.module('cheatsheet')
         function(csfUserSettings, csfStaticHighlight) {
             return {
                 restrict : 'E',
-                replace: true,
-                template: '<div></div>',
                 scope: {
                     textToHighlight: '@',
                     mode: '@'
                 },
                 link: function(scope, element, attrs) {
-                    scope.render = function() {
+                    var rendered = false;
+
+                    // Updates the styles and renders the element
+                    function render() {
                         element.html( csfStaticHighlight.render(scope.textToHighlight, scope.mode, scope.UserSettings.instance.editor) );
-                    };
-                    csfUserSettings.UserSettingsPromise.then(
-                        function(value) {
-                            scope.UserSettings = value;
-                            scope.$watch(
-                                'UserSettings.instance.editor',
-                                function(newValues, oldValues) {
-                                    scope.render();
-                                },
-                                true
-                            );
-                        }
-                    );
+                        rendered = true;
+                    }
+
+                    // Updates the styles only
+                    function updateStyles () {
+                        csfStaticHighlight.updateStyles(scope.UserSettings.instance.editor, element);
+                    }
+
+                    csfUserSettings.UserSettingsPromise.then(function(value) {
+                        scope.UserSettings = value;
+                        scope.$watch('UserSettings.instance.editor', rendered ? updateStyles : render, true);
+                        scope.$watchGroup( ['textToHighlight', 'mode'], render);
+                    });
 
                     scope.$on('$destroy', function() {
                     });
--- client/Services/csfStaticHighlight/Factories/csfStaticHighlight.js
+++ client/Services/csfStaticHighlight/Factories/csfStaticHighlight.js
@@ -1,32 +1,70 @@
 angular.module('cheatsheet')
-    .factory('csfStaticHighlight',
-        function($rootScope) {
+    .factory('csfStaticHighlight', [
+        'csfStaticHighlightBaseStyles',
+        function(csfStaticHighlightBaseStyles) {
             var cache = {
                 highlighter: ace.require("ace/ext/static_highlight"),
                 highlightModes: {},
-                themes: {}
+                themes: {},
+                currentEditorSettings: {},
+                baseStyles: csfStaticHighlightBaseStyles
             };
 
             return {
+                updateStyles: function(editorSettings, $element) {
+                    if(!editorSettings) {
+                        return;
+                    }
+
+                    if( !angular.equals(editorSettings, cache.currentEditorSettings) ) {
+                        // get/cache the theme
+                        cache.themes[editorSettings.theme] = cache.themes[editorSettings.theme] || ace.require(editorSettings.theme);
+
+                        // Augment the base styles with the theme styles
+                        var css = cache.baseStyles + cache.themes[editorSettings.theme];
+
+                        // Set the font size
+                        css = css.replace('font-size: 12px', 'font-size: ' + parseInt(editorSettings.fontSize, 10) + 'px');
+
+                        // remove old styles
+                        $('style#ace_highlight').remove();
+
+                        // add the new style
+                        $('head').append(
+                            $('<style>', {
+                                id: 'ace_highlight',
+                                text: css
+                            })
+                        );
+
+                        // Store the currently applied settings
+                        cache.currentEditorSettings = angular.copy(editorSettings);
+                    }
+
+                    if(!$element) {
+                        return;
+                    }
+
+                    // change the class of the element if supplied
+                    $element.children(':first')
+                        .removeClass()
+                        .addClass( cache.themes[editorSettings.theme].cssClass );
+                },
                 render: function(code, mode, editorSettings) {
                     if(!editorSettings) {
                         return '<div>User settings not found</div>';
                     }
+
                     cache.highlightModes[mode] = cache.highlightModes[mode] || new (ace.require(mode).Mode)();
                     cache.themes[editorSettings.theme] = cache.themes[editorSettings.theme] || ace.require(editorSettings.theme);
 
+                    this.updateStyles(editorSettings);
+
                     var highlighted = cache.highlighter.render(code.trim(), cache.highlightModes[mode], cache.themes[editorSettings.theme], 1, true);
-                    highlighted.css = highlighted.css.replace('font-size: 12px', 'font-size: ' + parseInt(editorSettings.fontSize, 10) + 'px');
-                    $('style#ace_highlight').remove();
-                    $('head').append(
-                        $('<style>', {
-                            id: 'ace_highlight',
-                            text: highlighted.css
-                        })
-                    );
+                    highlighted.session.destroy();
 
                     return highlighted.html;
                 }
             };
         }
-    );
+    ]);
