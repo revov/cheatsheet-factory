91f63bf1876eea5d53872e1470359aaecb781ef2
Stoyan Revov <st.revov@gmail.com>
2015-04-11 23:39:21 +0300

    csfCheatsheet code refactoring and improved permissions reactivity

--- client/Services/Cheatsheet/csfCheatsheet/Directives/csfCheatsheet.js
+++ client/Services/Cheatsheet/csfCheatsheet/Directives/csfCheatsheet.js
@@ -2,6 +2,85 @@ angular.module('cheatsheet')
     .directive('csfCheatsheet', [
         'csfUserSettings', '$compile', '$timeout', '$meteor',
         function(csfUserSettings, $compile, $timeout, $meteor) {
+            function pageDimmerHandler(scope, element, attrs) {
+                var dimmerElement = element.find('.dimmer');
+
+                dimmerElement.dimmer(
+                    {
+                        closable: false,
+                        duration: {
+                            show : 0,
+                            hide : 200
+                        }
+                    }
+                );
+                dimmerElement.dimmer('show');
+
+                scope.$on('$destroy', function() {
+                    dimmerElement.dimmer('destroy');
+                });
+            }
+
+            function rolePickerHandler(scope, element, attrs) {
+                scope.onEditPermissionAdded = function(role) {
+                    if( !_.contains(scope.component.meta.permissions.view, role) ) {
+                        scope.component.meta.permissions.view.push(role);
+                    }
+                };
+
+                scope.onViewPermissionRemoved = function(role) {
+                    if( _.contains(scope.component.meta.permissions.edit, role) ) {
+                        var index = scope.component.meta.permissions.edit.indexOf(role);
+                        scope.component.meta.permissions.edit.splice(index, 1);
+                    }
+                };
+            }
+
+            /**
+             * Get the name of the Author. TODO: Make this reactive
+             */
+            function userNamesSubscriptionHandler(scope, element, attrs) {
+                $meteor.subscribe('user-names', [scope.component.meta.userId])
+                    .then(function(value) {
+                        var authorSubscriptionHandle = value;
+
+                        var author = Meteor.users.findOne(scope.component.meta.userId);
+                        if(author) {
+                            scope.authorName = author.profile.firstName + ' ' + author.profile.lastName;
+                        }
+                        authorSubscriptionHandle.stop();
+                    });
+            }
+
+            function downloadButtonHandler(scope, element, attrs) {
+                var downloadButton = element.find('.csf-download-button');
+                downloadButton.on('mousedown', function() {
+                    if(!scope.component) {return;}
+
+                    var cheatsheetJson = JSON.stringify( Cheatsheets.findOne(scope.component._id) );
+                    downloadButton.attr( 'href', 'data:application/json;charset=utf-8,' + encodeURIComponent(cheatsheetJson) );
+                });
+            }
+
+            function permissionsHandler(scope, element, attrs) {
+                scope.$watch(
+                    'component.meta.permissions.edit',
+                    function() {
+                        scope.canI.edit = CanI.edit.cheatsheet(scope.component);
+                    }
+                );
+
+                var computation = Tracker.autorun(function(computation) {
+                    // If anything in the user changes, recalculate the permissions
+                    Meteor.user();
+                    scope.canI.edit = CanI.edit.cheatsheet(scope.component);
+                });
+
+                scope.$on('$destroy', function() {
+                    computation.stop();
+                });
+            }
+
             return {
                 restrict : 'E',
                 templateUrl: 'client/Services/Cheatsheet/csfCheatsheet/Templates/csfCheatsheet.ng.html',
@@ -9,16 +88,7 @@ angular.module('cheatsheet')
                     component: '='
                 },
                 link: function(scope, element, attrs) {
-                    var dimmerElement = element.find('.dimmer');
-
-                    dimmerElement.dimmer({
-                            closable: false,
-                            duration: {
-                                show : 0,
-                                hide : 200
-                            }
-                        })
-                        .dimmer('show');
+                    pageDimmerHandler(scope, element, attrs);
 
                     scope.canI = {
                         edit: false
@@ -43,20 +113,10 @@ angular.module('cheatsheet')
                             );
                             element.append( template );
                             $compile(template)(scope);
-                            dimmerElement.dimmer('hide');
+                            element.find('.dimmer').dimmer('hide');
                         }, 0);
 
-                        // Get the name of the Author. TODO: Make this reactive
-                        $meteor.subscribe('user-names', [scope.component.meta.userId])
-                            .then(function(value) {
-                                var authorSubscriptionHandle = value;
-
-                                var author = Meteor.users.findOne(scope.component.meta.userId);
-                                if(author) {
-                                    scope.authorName = author.profile.firstName + ' ' + author.profile.lastName;
-                                }
-                                authorSubscriptionHandle.stop();
-                            });
+                        userNamesSubscriptionHandler(scope, element, attrs);
                     }
 
                     //Wait until we have a resolved value for the component and compile after that
@@ -67,28 +127,9 @@ angular.module('cheatsheet')
                         }
                     });
 
-                    scope.$watch(
-                        'component.meta.permissions.edit',
-                        function() {
-                            scope.canI.edit = CanI.edit.cheatsheet(scope.component);
-                        }
-                    );
+                    permissionsHandler(scope, element, attrs);
 
-                    /**
-                     * Role picker fancy logic
-                     */
-                    scope.onEditPermissionAdded = function(role) {
-                        if( !_.contains(scope.component.meta.permissions.view, role) ) {
-                            scope.component.meta.permissions.view.push(role);
-                        }
-                    };
-
-                    scope.onViewPermissionRemoved = function(role) {
-                        if( _.contains(scope.component.meta.permissions.edit, role) ) {
-                            var index = scope.component.meta.permissions.edit.indexOf(role);
-                            scope.component.meta.permissions.edit.splice(index, 1);
-                        }
-                    };
+                    rolePickerHandler(scope, element, attrs);
 
                     /**
                      * Add new elements to the cheatsheet (will most often be just one container)
@@ -97,23 +138,13 @@ angular.module('cheatsheet')
                         scope.component.content.push(component);
                     };
 
-                    /**
-                     * Download button
-                     */
-                    var downloadButton = element.find('.csf-download-button');
-                    downloadButton.on('mousedown', function() {
-                        if(!scope.component) {return;}
-
-                        var cheatsheetJson = JSON.stringify( Cheatsheets.findOne(scope.component._id) );
-                        downloadButton.attr( 'href', 'data:application/json;charset=utf-8,' + encodeURIComponent(cheatsheetJson) );
-                    });
+                    downloadButtonHandler(scope, element, attrs);
 
                     /**
                      * Cleanup
                      */
                     scope.$on('$destroy', function() {
                         $timeout.cancel(compilationPromise);
-                        dimmerElement.dimmer('destroy');
                     });
 
                     element.on('$destroy', function() {
