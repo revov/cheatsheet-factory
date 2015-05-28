caeebcd5562b8f59c84a847a9a7058d445ba6a6c
Stoyan Revov <st.revov@gmail.com>
2015-05-28 18:46:27 +0300

    Make use of the filter (syntactic sugar) for

--- client/Services/csfUserSettings/Services/csfUserSettings.js
+++ client/Services/csfUserSettings/Services/csfUserSettings.js
@@ -13,14 +13,13 @@ angular.module('cheatsheet')
             subscription = Meteor.subscribe('user-settings');
             Tracker.autorun(function() {
                 if( Meteor.userId() && subscription.ready() ) {
-                    var userSettingsId = UserSettings.findOne({userId: Meteor.userId()}, {fields: {_id:true}})._id;
                     // Wrap the following inside $timeout since it contains reactive sources which should not trigger this autorun
                     $timeout(function() {
                         if(userSettings.instance) {
                             userSettings.instance.stop();
                         }
                         $rootScope.$apply(function() {
-                            userSettings.instance = $meteorObject(UserSettings, userSettingsId, false);
+                            userSettings.instance = $meteorObject(UserSettings, {userId: Meteor.userId()}, false);
                             deferred.resolve( userSettings );
                         });
                     });
