d8d90ef2c3d49a127141ace546a11068093e488d
Stoyan Revov <st.revov@gmail.com>
2015-06-06 17:12:41 +0300

    Added CreatedAt field

--- server/Collections/Cheatsheets/Publications.js
+++ server/Collections/Cheatsheets/Publications.js
@@ -1,25 +1,3 @@
-Cheatsheets.allow({
-    insert: function(userId, doc) {
-        return CanI.create.cheatsheet();
-    },
-    update: function(userId, doc, fieldNames, modifier) {
-        // Since Angular meteor updates objects by $set-ting all of their properties - this will be the only modifier we will allow
-        if( !_.every(_.keys(modifier), function(item) {return item==='$set'}) ) {
-            throw new Meteor.Error('403', 'Forbidden. Only $set modifier allowed.');
-        }
-
-        // Make sure someone does not attempt to modify meta.userId
-        if(modifier.$set.meta.userId !== doc.meta.userId) {
-            throw new Meteor.Error('403', 'Forbidden. Cannot change the author of the cheatsheet.');
-        }
-
-        return CanI.edit.cheatsheet(doc);
-    },
-    remove: function(userId, doc) {
-        return CanI.edit.cheatsheet(doc);
-    }
-});
-
 Meteor.publish('cheatsheets', function() {
     var cheatsheetsCursor = Cheatsheets.find( RestrictionsTo.view.cheatsheet(this.userId), {fields: {content: 0}} );
 
