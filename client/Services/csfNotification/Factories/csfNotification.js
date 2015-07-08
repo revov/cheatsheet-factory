18d11d1f5e2eaf7ce3d2ab155dff187e8238609a
Stoyan Revov <st.revov@gmail.com>
2015-07-08 03:03:20 +0300

    Refactoring leftovers

--- client/Services/csfNotification/Factories/csfNotification.js
+++ client/Services/csfNotification/Factories/csfNotification.js
@@ -1,11 +1,18 @@
 angular.module('cheatsheet')
-    .factory('csfNotification', [
-        '$rootScope',
-        function($rootScope) {
+    .factory('csfNotification',
+        function() {
+            toastr.options.showEasing = 'swing';
+            toastr.options.hideEasing = 'swing';
+            toastr.options.showMethod = 'slideDown';
+            toastr.options.hideMethod = 'slideUp';
+            toastr.options.timeOut = 10000;
+            toastr.options.showDuration = 150;
+            toastr.options.hideDuration = 300;
+
             return {
                 show: function(type, header, message) {
-                    $rootScope.$broadcast("notificationReceived", {type: type, header: header, message: message});
+                    toastr[type](message, header);
                 }
             };
         }
-    ]);
+    );
